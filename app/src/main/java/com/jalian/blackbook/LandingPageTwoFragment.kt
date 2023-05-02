13f8074e32b7519a3a4bdd961cdbec0c20c9739c
jlianf <jalianfebriandy15@gmail.com>
2023-05-02 21:33:00 +0700

    Add Fragment Two

--- app/src/main/java/com/jalian/blackbook/LandingPageTwoFragment.kt
+++ app/src/main/java/com/jalian/blackbook/LandingPageTwoFragment.kt
@@ -2,10 +2,26 @@ package com.jalian.blackbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jalian.blackbook.databinding.ActivityLandingPageTwoFragmentBinding
 
-class LandingPageTwoFragment : AppCompatActivity() {
+class LandingPageTwoFragment : Fragment() {
+    private var _binding : ActivityLandingPageTwoFragmentBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ActivityLandingPageTwoFragmentBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_landing_page_two_fragment)
     }
 }
