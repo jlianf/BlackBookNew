745c33e664c50265065cebbe34131d8677080b10
jlianf <jalianfebriandy15@gmail.com>
2023-05-08 11:52:21 +0700

    Fix error Gradle

--- app/src/main/java/com/jalian/blackbook/LandingActivity.kt
+++ app/src/main/java/com/jalian/blackbook/LandingActivity.kt
@@ -8,6 +8,8 @@ class LandingActivity : AppCompatActivity() {
     private val binding : ActivityLandingBinding by lazy {
         ActivityLandingBinding.inflate(layoutInflater)
     }
+
+    private lateinit var  landingAdapter: LandingAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -19,9 +21,11 @@ class LandingActivity : AppCompatActivity() {
 
         binding.btnVpNext.setOnClickListener{
             val currnetPosition = binding.vpLanding.currentItem
-            if (currnetPosition == LandingAdapter.itemCount - 1) {
-
-
+            if (currnetPosition == landingAdapter.itemCount - 1) {
+                intentTo(MainActivity::class.java)
+                finish()
+            } else {
+                binding.vpLanding.setCurrentItem(currnetPosition + 1, true)
             }
         }
     }
