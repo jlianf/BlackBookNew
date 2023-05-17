a139377437d34d4f50fad5a7d05a7cf63f465cb1
jlianf <jalianfebriandy15@gmail.com>
2023-05-17 18:38:40 +0700

    Fix activity main

--- app/src/main/java/com/jalian/blackbook/MainActivity.kt
+++ app/src/main/java/com/jalian/blackbook/MainActivity.kt
@@ -2,11 +2,24 @@ package com.jalian.blackbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.jalian.blackbook.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController : NavController
+
+    private val binding : ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val navHostFragment = binding.containerFragment.getFragment<NavHostFragment>()
+        navController = navHostFragment.navController
+        binding.buttomNav.setupWithNavController(navController)
 
     }
 }
